13525bdf96881f50cabfe374dd7a679085ad1939
Kevin <krobles48@outlook.com>
2019-10-25 08:54:46 -0600

    Login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,65 @@
 const express= require('express');
 const router = express.Router();
+const funcionario = require("../models/funcionario");
+const pasajero = require("../models/pasajero");
+
+router.post('/Indexapp',async(req,res)=>{
+    var correoEntrada = req.body.correo;
+    var contraseñaEntrada = req.body.contraseña;
+
+    console.log(correoEntrada);
+
+    var exito=[];
+    var errors =[];
+
+    var encontrado = 0;
+
+    await funcionario.findOne({correo:correoEntrada,contraseña:contraseñaEntrada}, async(err,funcio)=>{
+        if(err){
+            console.log(err);
+        }
+        if(!funcio){
+            //No lo encontro
+        }else{
+            if(funcio.tipo=="Administrador"){
+                res.render("./indexAdministrador",{
+                    correoEntrada});
+            }else{
+                //redireccionar otros funcionarios
+            }
+            encontrado = 1;
+            
+        }
+
+    })
+
+    await pasajero.findOne({correo:correoEntrada,contraseña:contraseñaEntrada}, async(err,pasaj)=>{
+        if(err){
+            console.log(err);
+        }
+        if(!pasaj){
+            //No lo encontro
+        }else{
+            //redireccionar
+            console.log("pasajero");
+            encontrado = 1;
+            
+        }
+    })
+    if(encontrado==0){
+        errors.push({text:"Usuario incorrecto"});
+        res.render("./indexapp",{
+            errors
+        })
+    }
+})
 
 
 router.get('/', (req,res)=>{
     res.render('Indexapp');
 })
 
-router.get('/administrador', (req,res)=>{
+router.get('/', (req,res)=>{
     res.render('indexAdministrador');
 })
 
